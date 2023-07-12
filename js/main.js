7c2000d26f26ad0ec18dcd08c41f85ee92fe3613
Diego Miranda <demiranda95@hotmail.com>
2023-07-12 12:02:55 -0400

    Inicio de Proyecto

---
+++ js/main.js
@@ -0,0 +1,11 @@
+var mainListDiv = document.getElementById("mainListDiv"),
+    mediaButton = document.getElementById("mediaButton");
+
+mediaButton.onclick = function () {
+    
+    "use strict";
+    
+    mainListDiv.classList.toggle("showList");
+    mediaButton.classList.toggle("active");
+    
+};
